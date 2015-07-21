de6ab655af09e54d50613a3a17bb02b02c6e4aac
zsx <zsx@zsxsoft.com>
2015-07-21 22:09:22 +0800

    Move converters to Converters.cs

---
+++ IFEOManage/Converters.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace IFEOManage
+{
+
+    /// <summary>
+    /// Inverse Boolean
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    /// <summary>
+    /// Convert ListView's index to Boolean
+    /// </summary>
+    [ValueConversion(typeof(int), typeof(bool))]
+    public class IndexToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(int))
+                throw new InvalidOperationException("The target must be a number");
+
+            System.Windows.MessageBox.Show((string)value);
+            return ((int)value == 0 ? false : true);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
