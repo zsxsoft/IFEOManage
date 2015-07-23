13712fdc70a5cf7fbdec2d75a4ab18687e0e926b
zsx <zsx@zsxsoft.com>
2015-07-23 10:32:41 +0800

    Add IFEOTest

---
+++ IFEOTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFEOTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IsAdministrator: " + IFEOGlobal.Global.IsAdministrator().ToString());
+            Console.WriteLine("CommandLine: " + Environment.CommandLine);
+            Console.WriteLine("CurrentDirectory: " + Environment.CurrentDirectory);
+            Console.WriteLine("Args: " + string.Join(", ", args));
+            Console.ReadLine();
+        }
+    }
+}
